3f950622604e4b75b8b885ecf27b0146304e0a1d
notebook <notebook@notebook-K53SV.(none)>
2012-10-08 21:00:38 +0300

    02_calculator - added test cases for multiply and factorial use cases, implemented all function as reaired in tests

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -13,3 +13,20 @@ def sum(args)
 	end
 	return result
 end
+
+def multiply(first, *args)
+	args.each do |argument|
+		first *= argument
+	end
+	return first
+end
+
+def power(value, power)
+	return value ** power
+end
+
+def factorial(value)
+	result = 1
+	value.times {|i| result *= i+1}
+	return result
+end
